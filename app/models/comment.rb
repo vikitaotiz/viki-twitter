1469964ca913d6b161d32acb96272d8fe22c2413
Victor Otieno <victorotieno598@gmail.com>
2021-06-29 20:51:29 +0300

    Add create method to controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+    validates :content, presence: true, length: { maximum: 100, too_long: '100 characters in comment is the maximum allowed.' }
+    belongs_to :user
+    belongs_to :tweet
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_171306) do
+ActiveRecord::Schema.define(version: 2021_06_29_174159) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_comments_on_tweet_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id"
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_06_29_171306) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "users"
 end
