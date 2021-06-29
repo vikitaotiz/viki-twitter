601efd17166942ce2450cd45be9c7835a59cda31
Victor Otieno <victorotieno598@gmail.com>
2021-06-29 20:59:42 +0300

    Add likes to tweets page and edit tweets controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :tweets
+  has_many :likes, dependent: :destroy
 
   attr_writer :login
   validate :validate_username
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_174159) do
+ActiveRecord::Schema.define(version: 2021_06_29_175240) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2021_06_29_174159) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["tweet_id"], name: "index_comments_on_tweet_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "tweet_id"
+    t.integer "user_id"
   end
 
   create_table "tweets", force: :cascade do |t|
@@ -44,4 +49,6 @@ ActiveRecord::Schema.define(version: 2021_06_29_174159) do
   end
 
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "tweets"
+  add_foreign_key "likes", "users"
 end
