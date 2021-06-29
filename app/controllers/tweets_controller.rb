5d623031edb4eea8a2c777cb8d3a8b4a34677dfb
Victor Otieno <victorotieno598@gmail.com>
2021-06-29 20:41:15 +0300

    Add create and show methods to tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,9 +4,27 @@ class TweetsController < ApplicationController
     timeline_tweets
   end
 
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def create
+    @tweet = current_user.tweets.new(tweet_params)
+
+    if @tweet.save
+      redirect_to tweets_path, notice: 'Tweet was successfully created.'
+    else
+      render :index, alert: 'Tweet was not created.'
+    end
+  end
+
   private
 
   def timeline_tweets
     @timeline_tweets = Tweet.all
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:content, :tweet_image)
   end
 end
